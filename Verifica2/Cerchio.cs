8f00c6b086c444c1609286dfbd7e94b61381ab13
Utente <Utente@LAPTOP-L0CG8TF2>
2024-04-07 17:16:36 +0200

    Aggiungere i file di progetto.

---
+++ Verifica2/Cerchio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verifica2
+{
+    internal class Cerchio
+    {
+        public double Raggio { get; set; }
+        public Cerchio() { }
+        public Cerchio(double raggio)
+        {
+            Raggio = raggio;
+        }
+        public override 
+
+        public static Cerchio operator +(Cerchio C, Cerchio other)
+        {
+            return new Cerchio(Math.Sqrt(Math.Pow(C.Raggio, 2) + Math.Pow(other.Raggio, 2)));
+        }
+    } public static Cerchio operator -(Cerchio C, Cerchio other)
+    {
+        if (C.Raggio > other.Raggio)
+        {
+            return new Cerchio(C.Raggio - other.Raggio);
+
+        }
+        else if (C.Raggio == other.Raggio)
+        {
+            return null;
+        }
+        else
+        {
+            return new Cerchio(other.Raggio - C.Raggio);
+        }
+    }  
+    public static bool operator ==(Cerchio C, Cerchio other)
+    {
+        if (object.ReferenceEquals(C, null))
+            return object.ReferenceEquals(other, null);
+        else if (object.ReferenceEquals(other, null))
+            return false;
+        else
+            return C.Raggio == other.Raggio;
+    }
+    public static bool operator != (Cerchio C, Cerchio other)
+    {
+        return !(C == other);
+    }
+    public static Cerchio Parse(string str)
+    {
+        if (!double.TryParse(str, out double raggio))
+        {
+            throw new ArgumentException();
+        }
+        else 
+        {
+            return new Cerchio(double.Parse(str));
+        }
+    }
+}
---
+++ Verifica2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verifica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio cerchio1 = Cerchio.Parse(Console.ReadLine());
+            Cerchio cerchio2 = Cerchio.Parse(Console.ReadLine());
+            Console.WriteLine("La somma: {0}", cerchio1 + cerchio2);
+            Console.ReadLine();
+
+        }
+    }
+}
